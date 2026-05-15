e4417ae8193a6bc7c56597dca36d9093b6661d6c
8bukets <120007961+8bukets@users.noreply.github.com>
2026-05-15 05:34:07 +0200

    docs: update compile definition (#1687)

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -58,11 +58,6 @@ export default function Home() {
     fetchData();
   }, []);
 
-  const [input, setInput] = useState("");
-  const [errorMessage, setErrorMessage] = useState<string | null>(null);
-  const { messages, sendMessage } = useChat({
-    onError: async (e) => setErrorMessage(e.message),
-  });
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
